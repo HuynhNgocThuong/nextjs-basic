460ae7cb27540be0afa1f07c2763caf13569b8b1
HuynhNgocThuong <huynhngocthuong0208@gmail.com>
2023-01-03 14:32:39 +0700

    [Modify] Learn navigation between pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter();
+  function goToDetailPage(){
+    router.push({
+      pathname: '/posts/[postId]',
+      query: {
+        postId: 123,
+        ref: 'social'
+      }
+    });
+  }
   return (
     <>
       <Head>
@@ -37,6 +49,10 @@ export default function Home() {
               />
             </a>
           </div>
+          <Link href="/about">
+            Go to about
+          </Link>
+          <button onClick={goToDetailPage}>detailPage</button>
         </div>
 
         <div className={styles.center}>
